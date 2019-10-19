b0e9aad397c53b778c7e6a2ae09e5e753318aa60
Gabarros <gabarros17@gmail.com>
2019-10-19 20:17:06 -0300

    Add PDFJS

---
+++ src/controller/DocumentPreviewController.js
@@ -0,0 +1,46 @@
+const pdfjsLib = require('pdfjs-dist');
+
+export class DocumentPreviewController{
+
+    constructor(file){
+
+        this._file = file;
+    }
+
+    getPreviewData(){
+
+        return new Promise((s, f)=>{
+
+            switch(this._file.type){
+
+                case 'image/jpg':           
+                case 'image/png':          
+                case 'image/jpeg':                  
+                case 'image/gif':
+
+                let reader = new FileReader();
+                reader.onload = e => {
+
+                    s({
+                        
+                        src: reader.result,
+                        info: this._file.name
+                    });
+
+
+                } 
+                reader.onerror = e =>{
+
+                    f(e);
+                }  
+                reader.readAsDataURL(this._file);
+                    break;
+                case 'application/pdf':
+                    break;
+                default:
+                    f();
+
+            }
+        });
+    }
+}
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
-import {Format} from './../util/Format';
-import {CameraController} from './CameraController';
+import { Format } from './../util/Format';
+import { CameraController } from './CameraController';
+import { DocumentPreviewController } from './DocumentPreviewController';
 
 export class WhatsAppController{
 
@@ -254,10 +255,64 @@ export class WhatsAppController{
             this.closeAllMainPanel();
             this.el.panelDocumentPreview.addClass('open');
 
-            // this.el.panelDocumentPreview.css({
-            //     'height':'calc(100% - 120px)'
-            // });
+            this.el.panelDocumentPreview.css({
+                 'height':'calc(100% - 120px)'
+            });
+
+            this.el.inputDocument.click();
         });
+
+        this.el.inputDocument.on('change', e=>{
+
+            if(this.el.inputDocument.files.length){
+
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file);
+
+                this._documentPreviewController.getPreviewData().then(result=>{
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show(); 
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err=>{
+
+                    switch(file.type){
+
+                        case 'application/vnd.oasis.opendocument.text':
+                        case 'application/msword':
+                                this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc'
+                        break;
+
+                        case 'application/vnd.ms-excel':
+                                this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls'
+
+                            break;
+   
+
+                        case 'application/vnd/ms-powerpoint':
+                                this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt'
+
+                            break;
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic'
+                            break;
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+
+                })
+
+                console.log(file);
+
+            }
+
+            
+
+        })
 
         this.el.btnClosePanelDocumentPreview.on('click', e=>{
 
