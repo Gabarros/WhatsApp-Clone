1462a924c99841f213f6eea66c9abc58ca73e1f0
Gabarros <gabarros17@gmail.com>
2019-10-24 23:04:56 -0300

    Add search contacts function

--- src/model/User.js
+++ src/model/User.js
@@ -73,11 +73,11 @@ export class User extends Model{
         .set(contact.toJson());
     }
 
-    getContacts(){
+    getContacts(filter = ''){
 
         return new Promise ((s, f)=>{
 
-                User.getContactsRef(this.email).onSnapshot(docs =>{
+                User.getContactsRef(this.email).where('name', '>=', filter).onSnapshot(docs =>{
 
                     let contacts = [];
 
