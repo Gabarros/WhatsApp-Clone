650af36c011d9f0045116778dd7efd5d09e1063e
Gabarros <gabarros17@gmail.com>
2019-10-26 00:30:52 -0300

    Send message to contact received

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -198,6 +198,8 @@ export class WhatsAppController {
                 message.fromJson(data);
 
                 let me = (data.from === this._users.email);
+
+                let view = message.getViewElement(me);
                 
                 if(!this.el.panelMessagesContainer.querySelector('#_'+data.id)){
                    
@@ -219,9 +221,12 @@ export class WhatsAppController {
 
                 } else{
 
-                    let view = message.getViewElement(me);
-                    this.el.panelMessagesContainer.querySelector('#_'+data.id).innerHTML = view.innerHTML;
+                  
 
+                    let parent = this.el.panelMessagesContainer.querySelector('#_'+data.id).parentNode;
+
+                    parent.replaceChild(view, this.el.panelMessagesContainer.querySelector('#_'+data.id));
+                  
                 }
                 
                 
@@ -232,7 +237,35 @@ export class WhatsAppController {
                     msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement()
                     .outerHTML;
 
-                }       
+                }    
+                
+                if(message.type === 'contact'){
+
+                    view.querySelector('.btn-message-send').on('click', e=>{
+
+                        Chat.createIfNotExists(this._user.email, message.content).then(chat => {
+
+
+                            let contact = new User(message.content.email);
+
+                            contact.on('datachange', data=>{
+
+                                contact.chatId = chat.id;
+
+                                this._user.addContact(contact);
+    
+                                this._user.chatId = chat.id;
+        
+                                contact.addContact(this._user);
+
+                                this.setActiveChat(contact);
+        
+                                
+                            })
+                           
+                        })
+                    })
+                };
             });
 
             if(autoScroll){
