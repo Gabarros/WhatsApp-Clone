6988da425624eafe24c83a59ea930392650dc18c
Gabarros <gabarros17@gmail.com>
2019-10-26 00:59:42 -0300

    Create upload audio functions

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -69,13 +69,25 @@ export class MicrophoneController extends ClassEvent{
 
                 let filename = `rec${Date.now()}.webm`;
 
-                let file = new File([blob], filename, {
-                    type: this._mimetype,
-                    lastModified: Date.now()
-                });
+                let audioContext = new AudioContext();
 
-                console.log('file', file);
+                let reader = new FileReader();
 
+                reader.onload = e=> {
+
+                    audioContext.decodeAudioData(reader.result).then(decode=>{
+
+                        let file = new File([blob], filename, {
+                            type: this._mimetype,
+                            lastModified: Date.now()
+                        });
+                    });
+
+                    this.trigger('recorded', file, decode);
+                   
+                }
+
+                reader.readAsArrayBuffer(blob);
             });
 
             this._mediaRecorder.start();
